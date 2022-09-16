ce994b85e3ae9bc1e7b6cbd412e6c6035986698a
Asthariel <yohann.fallas25@gmail.com>
2022-09-16 14:51:24 +0200

    v1sidebarrigth

--- src/components/RightBlock.js
+++ src/components/RightBlock.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../assets/components/rightblock.scss'
+import SidebarRight from '../components/SidebarRight'
 
 const RightBlock = () => {
     return (
         <div className='rightBlock'>
-            
+            <SidebarRight/>
         </div>
     );
 };
---
+++ src/components/SidebarRight.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../assets/components/sidebarright.scss'
+import '../assets/components/newsfeed.scss'
+import bbweed from '../assets/img/bbweed.png';
+
+const SidebarRight = () => {
+    return (
+        <div>
+            <div >
+                searchbar/bell/share
+            </div>
+            <div>
+                <p className="side-bar-title">les personnes à suivre</p>
+            </div>           
+            <div className="newsfeed-container-test">
+                <div className="profil">
+                    <img className="profil-thumbnail-img" src={bbweed} alt=""/>
+                    <p className="pseudo-right">Julian Speed</p>
+                        <div className="button-placement">
+                            <button className="subscribe">s'abonner</button>
+                        </div>                    
+                </div>
+            </div>         
+            <div className="newsfeed-container-test">
+                <div className="profil">
+                    <img className="profil-thumbnail-img" src={bbweed} alt=""/>
+                    <p className="pseudo-right">Eva Langoria</p>
+                        <div className="button-placement">
+                            <button className="subscribe">s'abonner</button>
+                        </div>                    
+                </div>
+            </div>          
+            <div className="newsfeed-container-test">
+                <div className="profil">
+                    <img className="profil-thumbnail-img" src={bbweed} alt=""/>
+                    <p className="pseudo-right">Clara Anagrom</p>
+                        <div className="button-placement">
+                            <button className="subscribe">s'abonner</button>
+                        </div>                    
+                </div>
+            </div>
+            <div className='line'></div>
+        </div>
+    );
+};
+
+export default SidebarRight;
