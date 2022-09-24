d7ebb7fb578afee143be631a7aa349de6cd78138
AidounNejma <guermachenejma@gmail.com>
2022-09-24 18:11:55 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,7 @@
-export const BASE_URL = 'https://127.0.0.1:8000';
-export const API_URL = 'https://127.0.0.1:8000/api';
+export const BASE_URL = process.env.API_URL;
+export const API_URL = BASE_URL + '/api';
 
 export const COMMENTS_API = API_URL + "comments";
 export const POSTS_API = API_URL + "posts";
 export const USERS_API = API_URL + "users";
 export const LOGIN_API = API_URL + "login_check";
-
